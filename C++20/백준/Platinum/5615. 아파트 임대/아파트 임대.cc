7163763e78182522e7b5921767fbe02a0e05e73c
DH.L <107869892+meozigoon@users.noreply.github.com>
2024-10-10 22:27:04 +0900

    [Platinum I] Title: 아파트 임대, Time: 448 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++20/백준/Platinum/5615. 아파트 임대/아파트 임대.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+unsigned long long p(unsigned long long x, unsigned long long y, unsigned long long m)
+{
+    unsigned long long res = 1;
+    x %= m;
+    while (y)
+    {
+        if (y & 1)
+        {
+            res *= x % m;
+            res %= m;
+        }
+        y /= 2;
+        x *= x % m;
+        x %= m;
+    }
+    return res;
+}
+
+bool mr(unsigned long long n, unsigned long long a)
+{
+    unsigned long long k = n - 1;
+    unsigned long long tmp;
+    while (1)
+    {
+        tmp = p(a, k, n);
+        if (tmp == n - 1)
+            return true;
+        if (k & 1)
+        {
+            if (tmp == 1)
+                return true;
+            else
+                return false;
+        }
+        k /= 2;
+    }
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+    unsigned long long t, cnt = 0;
+    cin >> t;
+    unsigned long long n;
+    for (int i = 0; i < t; i++)
+    {
+        cin >> n;
+        n = 2 * n + 1;
+        if (n == 7 || n == 61)
+            continue;
+        if (!mr(n, 2))
+            cnt++;
+        else if (!mr(n, 7))
+            cnt++;
+        else if (!mr(n, 61))
+            cnt++;
+    }
+    cout << t - cnt;
+    return 0;
+}
