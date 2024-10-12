33ed5128d4a836f8d14dea34111f495d27a87602
DH.L <107869892+meozigoon@users.noreply.github.com>
2024-10-12 18:25:02 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

---
+++ C++20/백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(NULL), cin.tie(NULL), cout.tie(NULL);
+	int n;
+	cin >> n;
+	queue<int> q;
+	for (int i = 0; i < n; i++)
+	{
+		q.push(i + 1);
+	}
+	while (q.size() > 1)
+	{
+		q.pop();
+		q.push(q.front());
+		q.pop();
+	}
+	cout << q.front();
+}
