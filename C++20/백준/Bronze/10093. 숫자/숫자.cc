a19e09cf229d0466d6da587adadb6a5bf0d0320f
DH.L <107869892+meozigoon@users.noreply.github.com>
2024-10-12 19:03:19 +0900

    [Bronze II] Title: 숫자, Time: 8 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ C++20/백준/Bronze/10093. 숫자/숫자.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    long long a, b;
+    cin >> a >> b;
+    if (a > b)
+        swap(a, b);
+    if (a == b || b - a == 1)
+        cout << 0;
+    else {
+        cout << b - a - 1 << '\n';
+        for (long long i = a + 1; i < b; i++)
+            cout << i << ' ';
+    }
+    return 0;
+}
