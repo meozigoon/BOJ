90d50e42437af4d007c1b3bcc7b35d0707c9d280
DH.L <107869892+meozigoon@users.noreply.github.com>
2024-10-12 18:37:22 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/5339. 콜센터/콜센터.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void)
+{
+    puts("     /~\\");
+    puts("    ( oo|");
+    puts("    _\\=/_");
+    puts("   /  _  \\");
+    puts("  //|/.\\|\\\\");
+    puts(" ||  \\ /  ||");
+    puts("============");
+    puts("|          |");
+    puts("|          |");
+    printf("|          |");
+    return 0;
+}
