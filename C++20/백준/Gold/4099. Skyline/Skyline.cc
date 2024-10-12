3106a724bd860f057b46f578f4c608d5fec86bea
DH.L <107869892+meozigoon@users.noreply.github.com>
2024-10-12 18:40:23 +0900

    [Gold III] Title: Skyline, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

---
+++ C++20/백준/Gold/4099. Skyline/Skyline.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+#include <stack>
+#include <numeric>
+
+using namespace std;
+
+long long dp[10001];
+
+long long c(long long n)
+{
+	if (!dp[n])
+	{
+		for (int i = 0; i < n; i++)
+		{
+			dp[n] += (c(i) * c(n - i - 1)) % 1000000;
+			dp[n] %= 1000000;
+		}
+	}
+	return dp[n];
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	int n;
+	dp[0] = dp[1] = 1;
+	while (1)
+	{
+		cin >> n;
+		if (!n)
+		    break;
+		cout << c(n) << '\n';
+	}
+	return 0;
+}
