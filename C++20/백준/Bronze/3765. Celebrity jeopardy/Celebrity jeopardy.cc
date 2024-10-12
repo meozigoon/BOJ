5987bace9e0012add7f8e1bedb314070ee3459a3
DH.L <107869892+meozigoon@users.noreply.github.com>
2024-10-12 18:40:12 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++20/백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+    string a;
+    while (cin >> a)
+        cout << a << '\n';
+    return 0;
+}
