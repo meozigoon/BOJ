8c89f32f459849c456d7fb891ce1fd5dc882b8dd
DH.L <107869892+meozigoon@users.noreply.github.com>
2024-10-12 18:26:43 +0900

    [Gold V] Title: 두 용액, Time: 60 ms, Memory: 2412 KB -BaekjoonHub

---
+++ C++20/백준/Gold/2470. 두 용액/두 용액.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+    int N;
+    cin >> N;
+    vector<int> answer(2);
+    vector<int> arr(N);
+    for (int i = 0; i < N; ++i)
+        cin >> arr[i];
+    sort(arr.begin(), arr.end());
+    int start = 0;
+    int end = N - 1;
+    int min = 2000000000;
+    while (start < end)
+    {
+        int sum = arr[start] + arr[end];
+        if (min > abs(sum))
+        {
+            min = abs(sum);
+            answer[0] = arr[start];
+            answer[1] = arr[end]; 
+            if (sum == 0)
+                break;
+        }
+        if (sum < 0) 
+            start++;
+        else 
+            end--;
+    }
+    cout << answer[0] << ' ' << answer[1];
+}
