0ad9f779fdec012c6d570898beec42fad9f6b7ad
DH.L <107869892+meozigoon@users.noreply.github.com>
2024-10-12 19:39:34 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/25083. 새싹/새싹.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void)
+{
+    puts("         ,r'\"7");
+    puts("r`-_   ,'  ,/");
+    puts(" \\. \". L_r'");
+    puts("   `~\\/");
+    puts("      |");
+    puts("      |");
+    return 0;
+}
