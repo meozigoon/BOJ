baa232f3eabb3726acba7befba07e19d6c2aa2bc
DH.L <107869892+meozigoon@users.noreply.github.com>
2024-10-12 19:42:36 +0900

    [Gold III] Title: 슬슬 가지를 먹지 않으면 죽는다, Time: 172 ms, Memory: 11368 KB -BaekjoonHub

---
+++ C++20/백준/Gold/27945. 슬슬 가지를 먹지 않으면 죽는다/슬슬 가지를 먹지 않으면 죽는다.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+#include <numeric>
+
+using namespace std;
+
+vector<int> link;
+
+int find(int x)
+{
+	if (x != link[x])
+		link[x] = find(link[x]);
+	return link[x];
+}
+
+bool same(int a, int b)
+{
+	return (find(a) == find(b));
+}
+
+void unite(int a, int b)
+{
+	a = find(a);
+	b = find(b);
+	if (a < b)
+		swap(a, b);
+	link[b] = a;
+	return;
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	int v, e;
+	int a, b, w;
+	int cnt = 1;
+	cin >> v >> e;
+	vector<tuple<int, int, int>> g;
+	for (int i = 0; i < e; i++)
+	{
+		cin >> a >> b >> w;
+		g.push_back({ w,a - 1,b - 1 });
+	}
+	sort(g.begin(), g.end());
+	link.resize(v);
+	iota(link.begin(), link.end(), 0);
+	for (auto& i : g)
+	{
+		tie(w, a, b) = i;
+		if (!same(a, b))
+		{
+			unite(a, b);
+			if (w != cnt)
+				break;
+			cnt++;
+		}
+	}
+	cout << cnt;
+	return 0;
+}
