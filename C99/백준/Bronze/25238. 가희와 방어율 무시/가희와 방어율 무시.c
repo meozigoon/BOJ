fa58614bf44ccd639952895daae03a53a7d4809e
DH.L <107869892+meozigoon@users.noreply.github.com>
2024-10-12 19:43:46 +0900

    [Bronze IV] Title: 가희와 방어율 무시, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/25238. 가희와 방어율 무시/가희와 방어율 무시.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(a*(100 - b)/100>=100)
+    {
+        printf("0");
+    }
+    else
+    {
+        printf("1");
+    }
+    return 0;
+}
