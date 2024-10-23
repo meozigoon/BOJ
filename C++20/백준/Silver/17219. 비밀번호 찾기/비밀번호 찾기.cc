f74ad7c037c66ccaf608883ecfc5e472518899b7
DH.L <107869892+meozigoon@users.noreply.github.com>
2024-10-24 00:50:37 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 4660 ms, Memory: 16548 KB -BaekjoonHub

---
+++ C++20/백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main(void)
+{
+    int n, m;
+    string id, pw;
+    map <string, string> account;
+    cin >> n >> m;
+    for(int i=0; i<n; i++)
+    {
+        cin >> id >> pw;
+        account[id] = pw;
+    }
+    for(int i=0; i<m; i++)
+    {
+        cin >> id;
+        cout << account[id] << '\n';
+    }
+}
