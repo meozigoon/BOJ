15111f3f3a5001ec687d979ac22782e5818aef3c
DH.L <107869892+meozigoon@users.noreply.github.com>
2024-10-24 01:10:33 +0900

    [Bronze V] Title: Serca, Time: 156 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++20/백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cout << " @@@   @@@ " << '\n';
+        cout << "@   @ @   @" << '\n';
+        cout << "@    @    @" << '\n';
+        cout << "@         @" << '\n';
+        cout << " @       @ " << '\n';
+        cout << "  @     @  " << '\n';
+        cout << "   @   @   " << '\n';
+        cout << "    @ @    " << '\n';
+        cout << "     @     " << '\n';
+    }
+}
