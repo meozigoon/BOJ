fdfda4b1f365770d33cd6c10e5072380ea7dbe48
DH.L <107869892+meozigoon@users.noreply.github.com>
2024-10-23 23:47:23 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++20/백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int arr[1001], n, m, ans = 0, k = 1;
+	for (int i = 1; i <= 1000; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			arr[k] = i;
+			if (k > 1000)
+			{
+				break;
+			}
+			k++;
+		}
+	}
+	cin >> n >> m;
+	for (int i = n; i <= m; i++)
+	{
+		ans += arr[i];
+	}
+	cout << ans;
+}
