aadb58d0005812db78896d533afab29c848e57be
DH.L <107869892+meozigoon@users.noreply.github.com>
2024-10-24 00:39:03 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++20/백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	string a;
+	cin >> a;
+	cout << ":fan::fan::fan:" << endl;
+	cout << ":fan::" << a << "::fan:" << endl;
+	cout << ":fan::fan::fan:";
+}
