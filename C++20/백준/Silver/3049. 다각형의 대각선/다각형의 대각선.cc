17fda47f7fc47894180a262d18a1204cffd6305e
DH.L <107869892+meozigoon@users.noreply.github.com>
2024-10-24 00:04:39 +0900

    [Silver V] Title: 다각형의 대각선, Time: 0 ms, Memory: 9848 KB -BaekjoonHub

---
+++ C++20/백준/Silver/3049. 다각형의 대각선/다각형의 대각선.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+long long c[1001][1001];
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	int n;
+	cin >> n;
+	c[0][0] = 1;
+	c[1][0] = 1;
+	c[1][1] = 1;
+	if (n < 4)
+		cout << 0;
+	else
+	{
+		for (int i = 2; i <= n; i++)
+			for (int j = 0; j <= i; j++)
+			{
+				if (j == i || j == 0)
+					c[i][j] = 1;
+				else
+					c[i][j] = (c[i - 1][j - 1] + c[i - 1][j]);
+			}
+		cout << c[n][4];
+	}
+}
