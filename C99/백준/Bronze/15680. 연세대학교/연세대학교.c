caaab359a8fc47952a8fcbd86dd16902d9a0ae82
DH.L <107869892+meozigoon@users.noreply.github.com>
2024-10-24 00:42:49 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/15680. 연세대학교/연세대학교.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n==0)
+    {
+        printf("YONSEI");
+    }
+    if(n==1)
+    {
+        printf("Leading the Way to the Future");
+    }
+    return 0;
+}
