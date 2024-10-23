66d091a8105d1d7b50bb6c5df5568346fb87597f
DH.L <107869892+meozigoon@users.noreply.github.com>
2024-10-24 00:38:59 +0900

    [Silver I] Title: 현수막, Time: 8 ms, Memory: 9032 KB -BaekjoonHub

---
+++ C++20/백준/Silver/14716. 현수막/현수막.cc
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+int map[250][250];
+bool vis[250][250];
+
+void dfs(int a, int b)
+{
+	vis[a][b] = true;
+	if (a + 1 < n)
+	{
+		if (map[a + 1][b] && !vis[a + 1][b])
+			dfs(a + 1, b);
+		if (b + 1 < m)
+			if (map[a + 1][b + 1] && !vis[a + 1][b + 1])
+				dfs(a + 1, b + 1);
+		if (b - 1 >= 0)
+			if (map[a + 1][b - 1] && !vis[a + 1][b - 1])
+				dfs(a + 1, b - 1);
+	}
+	if (a - 1 >= 0)
+	{
+		if (map[a - 1][b] && !vis[a - 1][b])
+			dfs(a - 1, b);
+		if (b + 1 < m)
+			if (map[a - 1][b + 1] && !vis[a - 1][b + 1])
+				dfs(a - 1, b + 1);
+		if (b - 1 >= 0)
+			if (map[a - 1][b - 1] && !vis[a - 1][b - 1])
+				dfs(a - 1, b - 1);
+	}
+	if (b + 1 < m)
+	{
+		if (map[a][b + 1] && !vis[a][b + 1])
+			dfs(a, b + 1);
+		if (a + 1 < m)
+			if (map[a + 1][b + 1] && !vis[a + 1][b + 1])
+				dfs(a + 1, b + 1);
+		if (a - 1 >= 0)
+			if (map[a - 1][b + 1] && !vis[a - 1][b + 1])
+				dfs(a - 1, b + 1);
+	}
+	if (b - 1 >= 0)
+	{
+		if (map[a][b - 1] && !vis[a][b - 1])
+			dfs(a, b - 1);
+		if (a + 1 < m)
+			if (map[a + 1][b - 1] && !vis[a + 1][b - 1])
+				dfs(a + 1, b - 1);
+		if (a - 1 >= 0)
+			if (map[a - 1][b - 1] && !vis[a - 1][b - 1])
+				dfs(a - 1, b - 1);
+	}
+	return;
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	int cnt;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+		{
+			cin >> map[i][j];
+			vis[i][j] = false;
+		}
+	cnt = 0;
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			if (!vis[i][j] && map[i][j])
+			{
+				cnt++;
+				dfs(i, j);
+			}
+	cout << cnt;
+}
