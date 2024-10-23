33c01819e9625a132312616d0ddae2b3dfe144f1
DH.L <107869892+meozigoon@users.noreply.github.com>
2024-10-23 23:48:04 +0900

    [Bronze I] Title: 뒤집힌 덧셈, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++20/백준/Bronze/1357. 뒤집힌 덧셈/뒤집힌 덧셈.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+ 
+int main(void) 
+{
+	int x, y;
+	int sum;
+	string a;
+	string b;
+	string result;
+	cin >> a >> b;
+	reverse(a.begin(), a.end());
+	reverse(b.begin(), b.end());
+	x = stoi(a);
+	y = stoi(b);
+	sum = x + y;
+	result = to_string(sum);
+	reverse(result.begin(), result.end());
+	cout << stoi(result) << endl;
+	return 0;
+}
