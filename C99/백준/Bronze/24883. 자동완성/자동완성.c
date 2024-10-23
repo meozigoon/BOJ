79a07394301c38599c3d31ff621d06303fb38772
DH.L <107869892+meozigoon@users.noreply.github.com>
2024-10-24 01:08:35 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/24883. 자동완성/자동완성.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    char c;
+    scanf("%c",&c);
+    if(c=='N'||c=='n')
+    {
+        printf("Naver D2");
+    }
+    else
+    {
+        printf("Naver Whale");
+    }
+    return 0;
+}
