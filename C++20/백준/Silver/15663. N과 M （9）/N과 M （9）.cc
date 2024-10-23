8763c31d18f2093d9b3685126362d0a02c7a6eb0
DH.L <107869892+meozigoon@users.noreply.github.com>
2024-10-24 00:41:54 +0900

    [Silver II] Title: N과 M (9), Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++20/백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+int arr[9];
+bool vis[9];
+int v[9];
+
+void dfs(int num, int cnt)
+{
+    if (cnt == m)
+    {
+        for (int i = 0; i < m; i++)
+            cout << arr[i] << ' ';
+        cout << '\n';
+        return;
+    }
+    int k = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (!vis[i] && k != v[i])
+        {
+            vis[i] = true;
+            arr[cnt] = v[i];
+            k = arr[cnt];
+            dfs(i + 1, cnt + 1);
+            vis[i] = false;
+        }
+    }
+    return;
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+        cin >> v[i];
+    sort(v, v + n + 1);
+    dfs(1, 0);
+}
