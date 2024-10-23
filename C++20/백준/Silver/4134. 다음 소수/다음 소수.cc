eac73ca4c36f60a2a5ba53c1254245ae7843183e
DH.L <107869892+meozigoon@users.noreply.github.com>
2024-10-24 00:06:11 +0900

    [Silver IV] Title: 다음 소수, Time: 304 ms, Memory: 2032 KB -BaekjoonHub

---
+++ C++20/백준/Silver/4134. 다음 소수/다음 소수.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+bool isPrimeNum(long long n)
+{
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+return true;
+}
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+    int T;
+    cin >> T;
+    long long n;
+    for (int i = 0; i < T; i++)
+    {
+        cin >> n;
+        if (n >= 0 && n <= 2)
+            cout << 2 << endl;
+        else if (n == 3)
+            cout << 3 << endl;
+        else
+        {
+            while (!isPrimeNum(n))
+                n++;
+            cout << n << endl;
+        }
+    }
+}
