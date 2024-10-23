8d8f7a2c433eb6d6911933a64526798bbd219e4d
DH.L <107869892+meozigoon@users.noreply.github.com>
2024-10-24 00:00:46 +0900

    [Bronze II] Title: 초6 수학, Time: 40 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++20/백준/Bronze/2702. 초6 수학/초6 수학.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int n, int m)
+{
+	if (m > n)
+		swap(n, m);
+	if (m == 0)
+		return n;
+	return gcd(m, n % m);
+}
+
+int main(void)
+{
+	int a, b;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+		cout << (a * b) / gcd(a, b) << ' ' << gcd(a, b) << endl;
+	}
+}
