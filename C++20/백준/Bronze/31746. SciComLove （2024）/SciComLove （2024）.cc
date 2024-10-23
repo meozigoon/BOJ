32c82362649154c50f47bf427f7e1de2879ca5ee
DH.L <107869892+meozigoon@users.noreply.github.com>
2024-10-23 23:01:22 +0900

    [Bronze IV] Title: SciComLove (2024), Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ C++20/백준/Bronze/31746. SciComLove （2024）/SciComLove （2024）.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	int n;
+	cin >> n;
+	if (n & 1)
+		cout << "evoLmoCicS";
+	else
+		cout << "SciComLove";
+	return 0;
+}
