1534e2b6d5cd6b5f8e1016426b9f05e3f7f926ea
DH.L <107869892+meozigoon@users.noreply.github.com>
2024-10-24 00:44:17 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/15963. CASIO/CASIO.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,m;
+    scanf("%d %d",&n,&m);
+    if(n==m)
+    {
+        printf("1");
+    }
+    if(n!=m)
+    {
+        printf("0");
+    }
+    return 0;
+}
