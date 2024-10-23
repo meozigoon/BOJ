46e63e93d6f90d759a6b8626252ca63dd37ed120
DH.L <107869892+meozigoon@users.noreply.github.com>
2024-10-24 00:15:06 +0900

    [Gold IV] Title: Bad Cowtractors, Time: 8 ms, Memory: 4456 KB -BaekjoonHub

---
+++ C++20/백준/Gold/7044. Bad Cowtractors/Bad Cowtractors.cc
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+
+using namespace std;
+
+vector<long long> link;
+
+long long find(long long x)
+{
+	if (x != link[x])
+		link[x] = find(link[x]);
+	return link[x];
+}
+
+bool same(long long a, long long b)
+{
+	return (find(a) == find(b));
+}
+
+void unite(long long a, long long b)
+{
+	a = find(a);
+	b = find(b);
+	if (a < b)
+		swap(a, b);
+	link[b] = a;
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	long long v, e;
+	cin >> v >> e;
+	vector<tuple<long long, long long, long long>> g;
+	long long a, b, w;
+	long long ans = 0;
+	for (long long i = 0; i < e; i++)
+	{
+		cin >> a >> b >> w;
+		g.push_back({ w,b - 1,a - 1 });
+		g.push_back({ w,a - 1,b - 1 });
+	}
+	sort(g.begin(), g.end(), greater<>());
+	for (long long i = 0; i < v; i++)
+		link.push_back(i);
+	for (long long i = 0; i < 2 * e; i++)
+	{
+		tie(w, a, b) = g[i];
+		if (!same(a, b))
+		{
+			unite(a, b);
+			ans += w;
+		}
+	}
+	bool ch = true;
+	for (int i = 0; i < v; i++)
+		if (find(i) != find(0))
+		{
+			ch = false;
+			break;
+		}
+	if (!ch)
+		cout << -1;
+	else
+		cout << ans;
+	return 0;
+}
