ae6c01a757974a3c009ac8053c3d31545158fbaf
DH.L <107869892+meozigoon@users.noreply.github.com>
2024-10-31 20:05:29 +0900

    [Bronze IV] Title: 치즈버거 만들기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++20/백준/Bronze/30017. 치즈버거 만들기/치즈버거 만들기.cc
+++ C++20/백준/Bronze/30017. 치즈버거 만들기/치즈버거 만들기.cc
@@ -1,15 +1,2 @@
-#include <iostream>
-#include <string>
-#include <vector>
-
-using namespace std;
-
-int main(void)
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL), cout.tie(NULL);
-	int a, b;
-	cin >> a >> b;
-	cout << min(a - 1, b) * 2 + 1;
-	return 0;
-}
+#include<iostream>
+int main(){int a,b;std::cin>>a>>b;std::cout<<std::min(a-1,b)*2+1;}
