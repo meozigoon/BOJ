076847216a0d875c4b5f57785ac26997eab4755b
DH.L <107869892+meozigoon@users.noreply.github.com>
2024-11-02 21:50:43 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++20/백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr), cout.tie(nullptr);
+	int n;
+	cin >> n;
+	while (n--)
+		cout << "LoveisKoreaUniversity ";
+	return 0;
+}
